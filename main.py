0a2643602ca06c82af373c70ff423755cb79351d
Chiragbachani1608 <107796393+Chiragbachani1608@users.noreply.github.com>
2023-03-17 01:01:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from weapon import *
 from sound import *
 from pathfinding import *
 
-GAME_NAME = "Shooting game made by Chirag Bachani"
+GAME_NAME = "Steel Sentinel game made by Chirag Bachani"
 
 class Game:
     def __init__(self):
